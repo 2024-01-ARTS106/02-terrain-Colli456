37dff8e69f33f9b31eeaa0bb9eda33aecd65fe19
Colli456 <cjclarke14y@gmail.com>
2024-03-10 09:38:35 -0400

    Win/Lose Score

---
+++ Assets/ScoreManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+
+    public static ScoreManager instance;
+
+    public int amount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Debug.LogError("Duplicated ScoreManager, ingnore this one", gameObject);
+        }
+    }
+}
